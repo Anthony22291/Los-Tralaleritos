26d8fe026f06ca7a527a38be9f90cd035c21b6f3
Kevin <frk0029715@est.univalle.edu>
2025-10-29 11:53:13 -0400

    intento de fondo animado , prueba de gift , scripts de los botones

--- Assets/02_Scripts/Menu/MenuPrincipal.cs
+++ Assets/02_Scripts/Menu/MenuPrincipal.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuPrincipal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void BtnJugar()
     {
-        
+        SceneManager.LoadScene("El-Despertar");
     }
 
-    // Update is called once per frame
-    void Update()
+ 
+   public void BtnSalir()
     {
-        
+        Debug.Log("Salir del juego");
+        Application.Quit();
+            
     }
 }
